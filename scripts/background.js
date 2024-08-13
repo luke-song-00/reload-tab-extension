d62c1e945308c09772b499211de1a54fbb8fb980
luke.song <luke.song.00@gmail.com>
2024-08-13 15:56:43 +0900

    add onUpdated with storage permissions

--- scripts/background.js
+++ scripts/background.js
@@ -1,19 +1,48 @@
-chrome.runtime.onStartup.addListener(() => {
-  console.log("!!!");
-});
-
 const naver = "https://www.naver.com";
 
-function runInterval() {
+function runInterval(intervalTime) {
   const INTERVAL_TIME = 3_000;
 
   window.refreshIntervalId = setInterval(() => {
     window.location.reload();
-  }, INTERVAL_TIME);
+  }, intervalTime || INTERVAL_TIME);
 }
 function removeInterval() {
   clearInterval(window.refreshIntervalId);
 }
+
+const STORAGE_KEY = "REFRESH_EXT_STATE";
+
+function executeScriptByState(state, tab) {
+  if (state === "ON") {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: runInterval,
+    });
+  } else {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: removeInterval,
+    });
+  }
+}
+
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  if (!tab.url.startsWith(naver)) return;
+
+  if (changeInfo.status == "complete") {
+    // do your things
+    const storage = await chrome.storage.session.get();
+    const prevState = storage[STORAGE_KEY] || "OFF";
+
+    await chrome.action.setBadgeText({
+      tabId: tab.id,
+      text: prevState,
+    });
+
+    executeScriptByState(prevState, tab);
+  }
+});
 
 chrome.action.onClicked.addListener(async (tab) => {
   if (tab.url.startsWith(naver)) {
@@ -21,6 +50,8 @@ chrome.action.onClicked.addListener(async (tab) => {
     const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
     // Next state will always be the opposite
     const nextState = prevState === "ON" ? "OFF" : "ON";
+    // set sessionStorage.
+    await chrome.storage.session.set({ [STORAGE_KEY]: nextState });
 
     // Set the action badge to the next state
     await chrome.action.setBadgeText({
@@ -28,16 +59,6 @@ chrome.action.onClicked.addListener(async (tab) => {
       text: nextState,
     });
 
-    if (nextState === "ON") {
-      chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        func: runInterval,
-      });
-    } else {
-      chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        func: removeInterval,
-      });
-    }
+    executeScriptByState(nextState, tab);
   }
 });
